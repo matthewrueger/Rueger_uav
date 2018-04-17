e07f0ed45d92ceb8f8f92bda7d4def50b81406ac
Matthew <matthewrueger@gmail.com>
2018-04-17 12:44:15 -0500

    test 1
    test 1

---
+++ UAVStep1.m
@@ -0,0 +1,42 @@
+%% Main Script for Puerto Rico UAV Disaster Response Fleet 
+clear all; close all; format long;
+
+%% SIMULATION PARAMETERS
+speed(1,1)=50;  %% slower UAV speed 
+speed(2,1)=100; %% faster UAV speed
+speed(3,1)=50;  %% slower UAV speed 
+
+%% Create Map
+figure
+MAP=imread('PuertoRico.png'); image(MAP); axis=[0 900 100 450]; hold on;  
+rect=[150 200 400 150];
+%% Create new UAV fleet 
+clear PRfleet;
+PRFleet=fleetManager; %% manages Puerto Rico UAV fleet
+PRFleet.map='PuertoRico.png'; %% UAV Fleet is deployed to Puerto Rico
+PRFleet.numUAVS=3;       %%Size of UAV fleet
+
+%% Specify Base Locations
+PRFleet.base(1,1)=700; PRFleet.base(1,3)=150;  %Base 1 
+PRFleet.base(2,1)=100; PRFleet.base(2,3)=300;  %Base 2 
+PRFleet.base(3,1)=650; PRFleet.base(3,3)=400;  %Base 2 
+
+for i=1:PRFleet.numUAVS
+%% Assign UAV to bases 
+ PRFleet.UAV(i,1).ID=i; %% Create UAV
+ PRFleet.UAV(i,1).x=PRFleet.base(i,1); %%  position each UAV at a base 
+ PRFleet.UAV(i,1).y=PRFleet.base(i,2); %% 
+ PRFleet.UAV(i,1).y=PRFleet.base(i,3); %% 
+ 
+ %% Assign UAV speeds
+ PRFleet.UAV(i,1).speed=speed(i,1); 
+end
+
+%% Plot Bases and UAV positions
+for ID=1:PRFleet.numUAVS
+ PRFleet.plotBaseLocation(ID); %%first way to call a class method
+ hold on;
+ plotUAVLocation(PRFleet,ID);  %%econd way to call a class method
+ hold on;
+ end
+
